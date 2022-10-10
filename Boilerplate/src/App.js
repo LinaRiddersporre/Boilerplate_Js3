6aa79bbcf1c216466c9c190e6bb38b9d28b51f48
Lina Riddersporre <85893597+LinaRiddersporre@users.noreply.github.com>
2022-10-10 21:42:13 +0200

    boilerplate

---
+++ Boilerplate/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="divBody">
+      
+      <h1>Hello World</h1>
+      
+      </div>
+    )
+  }
+}
+
+export default App;
