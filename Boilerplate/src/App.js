93463bc58c8a84474ffec1df360f6c9121db7b19
Lina Riddersporre <85893597+LinaRiddersporre@users.noreply.github.com>
2022-10-25 09:09:46 +0200

    node

--- Boilerplate/src/App.js
+++ Boilerplate/src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
 
-
-
 class App extends React.Component{
   render(){
     return(
       <div className="divBody">
-      
-      <h1>Hello World</h1>
       
       </div>
     )
